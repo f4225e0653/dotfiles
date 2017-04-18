1afe779c20e292dff3c933c4a2c240dbf8072a57
f4225 e0653 <ian.k.alexander@protonmail.ch>
2017-04-18 03:50:10 -0700

    Update 00baloo.sh

--- 00baloo.sh
+++ 00baloo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # cron script to disarm baloo file indexer, suggested use is daily
 
-find /usr/bin -name "*baloo*" -print0 | xargs -P 2 -n 1 -0 xz -9evf
+find /usr/bin -name "*baloo*" -print0 | xargs -P $(nproc) -n 1 -0 xz -9evf
 chmod -v a-x /usr/bin/*baloo*
